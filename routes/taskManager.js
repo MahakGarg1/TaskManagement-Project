0ab778f1dece89218d4fb26d3990c7d26552e6e8
mahak.garg <sbgupta@WellDyneRx.com>
2023-10-25 12:23:31 +0530

    Initial commit

---
+++ routes/taskManager.js
@@ -0,0 +1,116 @@
+// Imports
+const express = require('express')
+const taskRoutes = require('express').Router();
+const taskData = require('../taskData.json');
+const validator = require('../helpers/validator');
+const path = require('path');
+const fs = require('fs');
+
+// Initializations
+taskRoutes.use(express.urlencoded({ extended: false }));
+taskRoutes.use(express.json());
+
+// Get all tasks with an option to filter with task completion status
+taskRoutes.get("/", (req, res) => {
+    if (req.query.completion === undefined) {
+        res.status(200).send(taskData);  
+    }
+    else {
+        let result;
+        switch (String(req.query.completion)) {
+            case "true":
+                result = taskData.tasks.filter((x) => { return x.completed == true });
+                break;
+            case "false":
+                result = taskData.tasks.filter((x) => { return x.completed == false });
+                break;
+        }
+
+        let taskDataModified = JSON.parse(JSON.stringify(taskData));
+        taskDataModified.tasks = result;
+        res.status(200).send(taskDataModified);
+    }
+});
+
+// Get a task by it's id
+taskRoutes.get("/:id", (req, res) => {
+    let allTasks = taskData.tasks;
+    let reqTaskID = req.params.id;
+
+    let result = allTasks.filter(x => x.id == reqTaskID);
+
+    res.status(200).send(result);
+});
+
+// Get tasks by their priority level
+taskRoutes.get("/priority/:level", (req, res) => {
+    let reqTaskPriority = req.params.level;
+
+    let result;
+    switch (reqTaskPriority) {
+        case "low":
+            result = taskData.tasks.filter((x) => { return x.priority === "low" });
+            break;
+        case "medium":
+            result = taskData.tasks.filter((x) => { return x.priority === "medium" });
+            break;
+        case "high":
+            result = taskData.tasks.filter((x) => { return x.priority === "high" });
+            break;
+    }
+
+    let taskDataModified = JSON.parse(JSON.stringify(taskData));
+    taskDataModified.tasks = result;
+    res.status(200).send(taskDataModified);
+});
+
+// Create a task with proper validations
+taskRoutes.post("/", (req, res) => {
+    let taskDetails = req.body;
+    let writePath = path.join(__dirname, '..', 'taskData.json');
+
+    if(validator.validateTaskInfo(taskDetails, taskData).status) {
+        let taskDataModified = JSON.parse(JSON.stringify(taskData));
+        taskDataModified.tasks.push(taskDetails);
+        fs.writeFileSync(writePath, JSON.stringify(taskDataModified), {encoding:'utf8', flag:'w'});
+        res.status(200).json(validator.validateTaskInfo(taskDetails, taskData));
+    }
+    else {
+        res.status(400).json(validator.validateTaskInfo(taskDetails, taskData))
+    }
+});
+
+// Update a task by it's id
+taskRoutes.put("/:id", (req, res) => {
+    let taskDetails = req.body;
+    let reqTaskID = req.params.id;
+    let writePath = path.join(__dirname, '..', 'taskData.json');
+
+    if (validator.validateUpdateTaskInfo(taskDetails).status) {
+        let taskDataModified = JSON.parse(JSON.stringify(taskData));
+        taskDataModified.tasks[reqTaskID - 1] = taskDetails;
+        fs.writeFileSync(writePath, JSON.stringify(taskDataModified), {encoding:'utf8', flag:'w'});
+        res.status(200).json(validator.validateUpdateTaskInfo(taskDetails));
+    }
+    else {
+        res.status(400).json(validator.validateUpdateTaskInfo(taskDetails));
+    }
+    
+});
+
+// Delete a task by it's id
+taskRoutes.delete("/:id", (req, res) => {
+    let reqTaskID = req.params.id;
+    let writePath = path.join(__dirname, '..', 'taskData.json');
+
+    let result = taskData.tasks.filter((x) => { 
+        return x.id.toString() !== reqTaskID });
+
+    let taskDataModified = JSON.parse(JSON.stringify(taskData));
+    taskDataModified.tasks = result;
+    fs.writeFileSync(writePath, JSON.stringify(taskDataModified), {encoding:'utf8', flag:'w'});
+    res.status(200).send(taskDataModified);
+});
+
+// Exports
+module.exports = taskRoutes;
